92431707aa26d040f242c63f7f9b11184794c11e
kepherogalo <kepher.ogalo@student.moringaschool.com>
2023-07-31 19:31:29 +0300

    React set up & homepage

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -22,4 +22,43 @@ function App() {
   );
 }
 
+
+
+import React from 'react';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>KEPLYN HOMES FOR SHORT & LONG STAY</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>Login</h2>
+          <LoginForm />
+        </section>
+
+        <section>
+          <h2>Sign Up</h2>
+          <SignupForm />
+        </section>
+
+        <section>
+          <h2>Contact Us</h2>
+          <ContactForm />
+        </section>
+      </main>
+
+      <footer>
+        <p>© 2023 House Rental Website. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
 export default App;
+
